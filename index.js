bb4520dfa11e882d1c1f937a5eed2bc7f57334e6
Flittis <mega.aang@gmail.com>
2019-03-29 02:02:42 +0200

    Fixed bug, when "/delall" not working properly.

--- index.js
+++ index.js
@@ -11,36 +11,42 @@ vk.longpoll.start();
 console.log("Longpoll started successfully!");
 
 // Keywords to delete
-let dellast = "oops"; // delete yours last message
+let dellast = "ой"; // delete yours last message
 let delall = "/delall"; // delete your messages for past 24 hours
+
+// Func to delete messages
+let delFunc = function(ids) {
+    vk.messages.delete({
+        delete_for_all: 1,
+        message_ids: ids.toString()
+    });
+}
 
 // Callback function
 vk.on("message", function(event, msg) {
 
     if(!msg.out) return;
+    if(msg.body.toLowerCase() != dellast && msg.body.toLowerCase() != delall) return;
 
     let thispeer = msg.peer_id;
-    var ids = [];
 
     vk.messages.getHistory({
         peer_id: thispeer,
         count: 200
     }).then((res) => {
-        if(msg.body.toLowerCase() == dellast)
+        var ids = [];
+
+        if(msg.body.toLowerCase() == dellast) {
               for(var x = 0; ids.length <= 1; x++) if(res.items[x].out == 1) ids.push(res.items[x].id);
-        else if(msg.body.toLowerCase() == delall) {
-            vk.utils.getServerTime().then((result) => {
-                for(var x = 0; x < res.items.length; x++) {
-                    subtract = result - res.items[x].date;
-                    if(res.items[x].out == 1 && subtract < 86000) ids.push(res.items[x].id);
-                }
-            });
+              delFunc(ids);
+        } else if(msg.body.toLowerCase() == delall) {
+              vk.utils.getServerTime().then((result) => {
+                  for(var x = 0; x < res.items.length; x++) {
+                      subtract = result - res.items[x].date;
+                      if(res.items[x].out && subtract < 86000) ids.push(res.items[x].id);
+                  }
+                  delFunc(ids);
+              });
         }
-
-        vk.messages.delete({
-            delete_for_all: 1,
-            message_ids: ids.toString()
-        });
     });
-
 })

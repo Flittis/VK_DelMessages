60d270c5fb037d6df125add67638aadd088576b9
Nikolay Flitov <mega.aang@gmail.com>
2019-11-02 14:45:28 +0200

    Now message gonna edit, if you write [-] with trigger word.

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
 // https://vk.cc/8E0H4r
 
 var VK = require("VK-Promise"),
-    vk = new VK(" < token here > ");
+    vk = new VK(" < token > ");
 
 // Starting longpoll server
 vk.longpoll.start();
 
 // Keywords to delete
-let dellast = "ой"; // delete yours last message
-let delall = "/delall"; // delete your messages for past 24 hours
+let dellast = "ой", // delete yours last message
+    delall = "/delall", // delete your messages for past 24 hours
+    editto = " "; // message will be edited to this text
 
-// Func to delete messages
-let delFunc = function(ids) {
-    vk.messages.delete({
-        delete_for_all: 1,
-        message_ids: ids.toString()
-    });
-}
+// Waiting for message...
+vk.on("message", onMessage);
 
-// Callback function
-vk.on("message", function(event, msg) {
-
+function onMessage(event, msg) {
     if(!msg.out) return;
     if(!msg.body.toLowerCase().startsWith(dellast) && msg.body.toLowerCase() != delall) return;
 
-    let thispeer = msg.peer_id;
+    vk.messages.getHistory({peer_id: msg.peer_id, count: 200})
+      .then((res) => {
+          var ids = [];
 
-    vk.messages.getHistory({
-        peer_id: thispeer,
-        count: 200
-    }).then((res) => {
-        var ids = [];
+          if(msg.body.toLowerCase().startsWith(dellast)) {
+                var lengthTo = 1, isEdit = false;
 
-        if(msg.body.toLowerCase().startsWith(dellast)) {
-              let num = msg.body.toLowerCase().replace(dellast, '').trim();
-              if(num && num.match(/[а-яёa-z]/g)) return;
+                if(msg.body.includes("-")) isEdit = true;
+                let num = msg.body.toLowerCase().replace(dellast, '').replace("-", "").trim();
+                if(num && num.match(/[0-9]+/g)) lengthTo = parseInt(num);
+                else if(num) return;
 
-              var lengthTo = 1;
-              if(num && num.match(/\d+/)) lengthTo = parseInt(num);
+                if(lengthTo < 1) return;
 
-              for(var x = 0; ids.length <= lengthTo; x++) if(res.items[x].out == 1) ids.push(res.items[x].id);
-              delFunc(ids);
-        } else if(msg.body.toLowerCase() == delall) {
-              vk.utils.getServerTime().then((result) => {
-                  for(var x = 0; x < res.items.length; x++) {
-                      subtract = result - res.items[x].date;
-                      if(res.items[x].out && subtract < 86000) ids.push(res.items[x].id);
-                  }
-                  delFunc(ids);
-              })
-        }
-    })
-})
+                for(var x = 0; ids.length <= lengthTo; x++) {
+                    if(res.items[x].out == 1) ids.push(res.items[x].id);
+
+                    if(isEdit && x != 0) vk.messages.edit({ peer_id: msg.peer_id, message_id: ids[x], message: editto });
+                }
+                delFunc(ids);
+          } else if(msg.body.toLowerCase() == delall) {
+                vk.utils.getServerTime().then((result) => {
+                    for(var x = 0; x < res.items.length; x++)
+                        if(res.items[x].out && (result - res.items[x].date) < 86000) ids.push(res.items[x].id);
+
+                    delFunc(ids);
+                });
+          }
+
+      });
+}
+
+// Func to delete messages
+function delFunc(ids) { vk.messages.delete({ delete_for_all: 1, message_ids: ids.toString() }); }
